6ff1f140934adfa7e8b494ddfe1888cb916b1bbc
Pawan Sharma <pawansharma93520@gmail.com>
2024-02-27 00:54:29 +0530

    a simple project is complete

---
+++ fizz_buzz.js
@@ -0,0 +1,20 @@
+var output = [];
+var count = 1;
+function fizzBuzz() {
+
+ 
+
+ if (count%3===0) {
+  output.push("fizz");
+ }else if(count%5===0){
+  output.push("fizzBuzz");
+ }
+ 
+ 
+ else {
+  output.push(count);
+ }
+  
+ count++;
+  console.log(output);
+}
