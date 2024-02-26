3db771f4d838c87044b0bafb9c11a025029c97fa
Pawan Sharma <pawansharma93520@gmail.com>
2024-02-27 01:03:13 +0530

    fixed some bugs

--- fizz_buzz.js
+++ fizz_buzz.js
@@ -1,20 +1,16 @@
-var output = [];
-var count = 1;
-function fizzBuzz() {
-
- 
-
- if (count%3===0) {
-  output.push("fizz");
- }else if(count%5===0){
-  output.push("fizzBuzz");
- }
- 
- 
- else {
-  output.push(count);
- }
-  
- count++;
-  console.log(output);
+var output = []
+var count = 1
+function fizzBuzz () {
+  if (count % 3 === 0 && count % 5 === 0) {
+    output.push('FizzBuzz')
+  }
+  else if (count % 3 === 0) {
+    output.push('fizz')
+  }else if (count % 5 === 0) {
+    output.push('Buzz')
+  }else {
+    output.push(count)
+  }
+  count++
+  console.log(output)
 }
